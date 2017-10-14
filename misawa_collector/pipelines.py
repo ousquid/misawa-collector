95f78819d71829580c4c9dc02173616b67715c52
ousquid <ousquid.share@gmail.com>
2017-10-14 13:26:29 +0000

    とりあえず完成

--- misawa_collector/pipelines.py
+++ misawa_collector/pipelines.py
@@ -4,8 +4,30 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import sys
+import os
+from PIL import Image
+from io import StringIO
+import requests
 
 class MisawaCollectorPipeline(object):
     def process_item(self, item, spider):
+        """ item: {'name':str, 'url'}
+        """
+        print("pipline kitayo-.")
+        # create name directory
+        img_dir = os.path.join('images', item['name'])
+        os.makedirs(img_dir, exist_ok=True)
+
+        # download image
+        #res = requests.get(item['url'])
+        #img_cnt = StringIO(res.text)
+        #img = Image.open(img_cnt)
+        res = requests.get(item['url'], stream=True)
+        img = Image.open(res.raw)
+
+        # create image name
+        img_name = item['url'].split('/')[-1]
+        img_path = os.path.join(img_dir, img_name)
+        img.save(img_path)
         return item
---
+++ misawa_collector/spiders/chara_spider.py
@@ -0,0 +1,22 @@
+# -*- coding=utf-8 -*-
+"""
+"""
+
+import scrapy
+
+class CharaSpider(scrapy.Spider):
+    name='chara'
+    start_urls=['http://jigokuno.com']
+
+    def parse(self, response):
+        for article in response.css("article"):
+            name = article.css("div.article-category a::text").extract_first() 
+            url = article.css("div.article-body-inner a::attr(href)").extract_first()
+            yield {
+                'name' : name,
+                'url' : url,
+            }
+            
+        next_page = response.xpath("//a[@rel='next']").extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
